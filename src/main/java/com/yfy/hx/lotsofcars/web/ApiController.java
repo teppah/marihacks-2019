680ff29411842f2f352444dc7b782e5d11315c48
teppah <yifengyang01@gmail.com>
2019-03-02 16:46:35 -0500

    added prelim method to pull data from grid

--- src/main/java/com/yfy/hx/lotsofcars/web/ApiController.java
+++ src/main/java/com/yfy/hx/lotsofcars/web/ApiController.java
@@ -1,9 +1,11 @@
 package com.yfy.hx.lotsofcars.web;
 
+import com.yfy.hx.lotsofcars.core.AbstractEntity;
 import com.yfy.hx.lotsofcars.core.Grid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +19,9 @@ public class ApiController {
         log.info("finished initialization");
     }
 
+    @GetMapping("api/grid")
+    public AbstractEntity[][] getGrid() {
+        return grid.grid;
+    }
 
 }
