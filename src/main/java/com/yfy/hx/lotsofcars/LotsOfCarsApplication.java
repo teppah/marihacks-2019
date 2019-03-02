2bb83512fd15ac241a35b9325f93fd784066dbcb
teppah <yifengyang01@gmail.com>
2019-03-02 15:42:30 -0500

    initial commit

---
+++ src/main/java/com/yfy/hx/lotsofcars/LotsOfCarsApplication.java
@@ -0,0 +1,13 @@
+package com.yfy.hx.lotsofcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotsOfCarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotsOfCarsApplication.class, args);
+    }
+
+}
