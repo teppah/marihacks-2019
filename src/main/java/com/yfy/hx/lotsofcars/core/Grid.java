188a6e7cca4a0ca58a5be059bc28f39ae9b43b8d
teppah <yifengyang01@gmail.com>
2019-03-02 16:46:24 -0500

    add to grid method

--- src/main/java/com/yfy/hx/lotsofcars/core/Grid.java
+++ src/main/java/com/yfy/hx/lotsofcars/core/Grid.java
@@ -2,7 +2,6 @@ package com.yfy.hx.lotsofcars.core;
 
 import org.springframework.stereotype.Component;
 
-@Component
 public class Grid {
     public AbstractEntity[][] grid = new AbstractEntity[40][20];
 
@@ -10,12 +9,29 @@ public class Grid {
     public static final int gridWidth = 40;
 
     public void print() {
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 40; j++) {
+        for (int i = 0; i < gridHeight; i++) {
+            for (int j = 0; j < gridWidth; j++) {
                 System.out.print(grid[j][i] + " ");
             }
             System.out.println();
         }
+    }
+
+
+    public void addThing(AbstractEntity entity, int x, int y) {
+        if (x < 0 || y < 0) {
+            throw new IllegalArgumentException("bro sumtins wrong: x = " + x + " and y = " + y);
+        }
+
+        else if (x >  gridWidth && y > gridHeight) {
+            throw new IllegalArgumentException("Invalid x and y coord: x = " + x + " y = " + y);
+        } else {
+            entity.x = x;
+            entity.y = y;
+            grid[x][y] = entity;
+        }
+
+
     }
 
 
