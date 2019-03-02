d74f148cda2c7ac9248f2e43b689c4b359435dfe
teppah <yifengyang01@gmail.com>
2019-03-02 16:28:03 -0500

    added main application runner

--- src/main/java/com/yfy/hx/lotsofcars/LotsOfCarsApplication.java
+++ src/main/java/com/yfy/hx/lotsofcars/LotsOfCarsApplication.java
@@ -1,5 +1,6 @@
 package com.yfy.hx.lotsofcars;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,6 @@ public class LotsOfCarsApplication {
     public static void main(String[] args) {
         SpringApplication.run(LotsOfCarsApplication.class, args);
     }
+
 
 }
