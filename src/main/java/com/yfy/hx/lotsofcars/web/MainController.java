65613f135f59899dcf9295a10f3563a9eff4a1a5
Laurence L <laurencel2001@gmail.com>
2019-03-02 20:09:50 -0500

    landing page

--- src/main/java/com/yfy/hx/lotsofcars/web/MainController.java
+++ src/main/java/com/yfy/hx/lotsofcars/web/MainController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController  {
 
+    @GetMapping("")
+    public String default_landing_page(){
+        return "landing";
+    }
+
     @GetMapping("home")
     public String hello() {
         return "main";
