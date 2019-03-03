7ddfd5d00237bbe7f47896202e1fd0fddd305a94
Gongsta <gong.steven@hotmail.com>
2019-03-03 09:33:50 -0500

    fixed error with currentER

--- src/main/java/com/yfy/hx/lotsofcars/core/Road.java
+++ src/main/java/com/yfy/hx/lotsofcars/core/Road.java
@@ -2,6 +2,8 @@ package com.yfy.hx.lotsofcars.core;
 
 public class Road extends AbstractEntity {
     public Car currentCar = null;
+    public ERvehicle currentER = null;
+    
     public Road(String orientation) {
         super(orientation);
     }
