ec485e8bc3bc64467e3d5f695e25577d1fe71c5a
teppah <yifengyang01@gmail.com>
2019-03-02 19:09:28 -0500

    added some landing stuff

--- src/main/java/com/yfy/hx/lotsofcars/web/MainController.java
+++ src/main/java/com/yfy/hx/lotsofcars/web/MainController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController  {
 
-    @GetMapping("/home")
+    @GetMapping("home")
     public String hello() {
         return "main";
+    }
+
+    @GetMapping("landing")
+    public String landing() {
+        return "landing";
+    }
+
+    @GetMapping("layout")
+    public String layout() {
+        return "layout";
     }
 }
